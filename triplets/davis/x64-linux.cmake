fcf7ac2b758870a25876e0a89ce559a9a98146f7
Daniel Maurice Davis <daniel.maurice.davis@gmail.com>
2025-10-19 22:09:13 -0400

    feat: add custom triplet to fix glog issues

---
+++ triplets/davis/x64-linux.cmake
@@ -0,0 +1,11 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+# https://github.com/microsoft/vcpkg/issues/37141
+if (${PORT} MATCHES "gflags|glog")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+    set(VCPKG_FIXUP_ELF_RPATH ON)
+endif()
