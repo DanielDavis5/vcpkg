6b88b7a6deb94b74825cb11f1dafb7236523a3a2
Daniel Maurice Davis <daniel.maurice.davis@gmail.com>
2025-12-05 22:40:22 -0500

    feat: don't bother with gflgs|glog

--- triplets/davis/x64-linux.cmake
+++ triplets/davis/x64-linux.cmake
@@ -4,10 +4,4 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-# https://github.com/microsoft/vcpkg/issues/37141
-if (${PORT} MATCHES "gflags|glog")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-    set(VCPKG_FIXUP_ELF_RPATH ON)
-endif()
-
 set(CPUINFO_LOG_LEVEL default)
